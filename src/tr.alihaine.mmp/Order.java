d7ef3f88447171c8d2a2e0ac342f1e191b0c4375
Ali Yagmur <alihainetr@gmail.com>
2021-07-06 18:30:01 +0200

    Ajout des boissons et accompagnement pour tous (Corrigé)

--- src/tr.alihaine.mmp/Order.java
+++ src/tr.alihaine.mmp/Order.java
@@ -4,14 +4,36 @@ import java.util.Scanner;
 
 public class Order {
     boolean menu = false;
-    boolean drink = true;
 
     public void runMenu() {
         while (!menu) {
             this.displayAvailableMenu();
             Scanner sc = new Scanner(System.in);
-            int nb = sc.nextInt();
-            this.displaySelectedMenu(nb);
+            int nbMenu = sc.nextInt();
+            this.displaySelectedMenu(nbMenu);
+            switch (nbMenu) {
+                case 1:
+                    displayAvailableSide(true);
+                    int nbSide = sc.nextInt();
+                    displaySelectedSide(nbSide, true);
+                    displayAvailableDrink();
+                    int nbDrink = sc.nextInt();
+                    displaySelectedDrink(nbDrink);
+                    break;
+                case 2:
+                    displayAvailableSide(true);
+                    nbSide = sc.nextInt();
+                    displaySelectedSide(nbSide, true);
+                    break;
+                case 3:
+                    displayAvailableSide(false);
+                    nbSide = sc.nextInt();
+                    displaySelectedSide(nbSide, false);
+                    displayAvailableDrink();
+                    nbDrink = sc.nextInt();
+                    displaySelectedDrink(nbDrink);
+                    break;
+            }
         }
     }
 
@@ -27,18 +49,14 @@ public class Order {
         switch (nbMenu) {
             case 1:
                 System.out.println("Vous avez choisi comme menu : poulet");
-                this.displayAvailableAccompagnementA();
                 menu = true;
                 break;
             case 2:
                 System.out.println("Vous avez choisi comme menu : boeuf");
-                drink = false;
-                this.displayAvailableAccompagnementA();
                 menu = true;
                 break;
             case 3:
                 System.out.println("Vous avez choisi comme menu : végétarien");
-                this.displayAvailableAccompagnementB();
                 menu = true;
                 break;
             default:
@@ -46,25 +64,19 @@ public class Order {
                 break;
         }
     }
-    public void displayAvailableAccompagnementA() {
-        System.out.println("Choix de l'accompagnement");
-        System.out.println("1 - Des légumes frais");
-        System.out.println("2 - Des frites");
-        System.out.println("3 - Du riz");
-        System.out.println("Que souhaitez-vous comme accompagnement ?");
-        Scanner sc = new Scanner(System.in);
-        int nb = sc.nextInt();
-        this.displaySelectedSide(nb, true);
-    }
-
-    public void displayAvailableAccompagnementB() {
-        System.out.println("Choix de l'accompagnement");
-        System.out.println("1 - Riz");
-        System.out.println("2 - Sans riz");
-        System.out.println("Que souhaitez-vous comme accompagnement ?");
-        Scanner sc = new Scanner(System.in);
-        int nb = sc.nextInt();
-        this.displaySelectedSide(nb, false);
+    public void displayAvailableSide(boolean allSideEnable) {
+        if (allSideEnable) {
+            System.out.println("Choix de l'accompagnement");
+            System.out.println("1 - Des légumes frais");
+            System.out.println("2 - Des frites");
+            System.out.println("3 - Du riz");
+            System.out.println("Que souhaitez-vous comme accompagnement ?");
+        } else {
+            System.out.println("Choix de l'accompagnement");
+            System.out.println("1 - Riz");
+            System.out.println("2 - Sans riz");
+            System.out.println("Que souhaitez-vous comme accompagnement ?");
+        }
     }
 
     public void displaySelectedSide(int i, boolean b) {
@@ -72,15 +84,12 @@ public class Order {
             switch (i) {
                 case 1:
                     System.out.println("Vous avez choisi comme accompagnement : légumes frais");
-                    this.displayAvailableDrink();
                     break;
                 case 2:
                     System.out.println("Vous avez choisi comme accompagnement : frites");
-                    this.displayAvailableDrink();
                     break;
                 case 3:
                     System.out.println("Vous avez choisi comme accompagnement : riz");
-                    this.displayAvailableDrink();
                     break;
                 default:
                     System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
@@ -90,11 +99,9 @@ public class Order {
             switch (i) {
                 case 1:
                     System.out.println("Vous avez choisi comme accompagnement : riz");
-                    this.displayAvailableDrink();
                     break;
                 case 2:
                     System.out.println("Vous avez choisi comme accompagnement : pas de riz");
-                    this.displayAvailableDrink();
                     break;
                 default:
                     System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
@@ -105,31 +112,26 @@ public class Order {
     }
 
     public void displayAvailableDrink() {
-        if (drink) {
             System.out.println("Choix de la boisson");
             System.out.println("1 - Eau plate");
             System.out.println("2 - Eau gazeuse");
             System.out.println("3 - Soda");
             System.out.println("Que souhaitez-vous comme boisson ?");
-            Scanner sc = new Scanner(System.in);
-            int nb = sc.nextInt();
-            this.displaySelectedDrink (nb);
-        }
     }
 
     public void displaySelectedDrink (int i) {
         switch (i) {
             case 1:
-                System.out.println("Vous avez choisi comme accompagnement : eau plate");
+                System.out.println("Vous avez choisi comme boisson : eau plate");
                 break;
             case 2:
-                System.out.println("Vous avez choisi comme accompagnement : eau gazeuse");
+                System.out.println("Vous avez choisi comme boisson : eau gazeuse");
                 break;
             case 3:
-                System.out.println("Vous avez choisi comme accompagnement : soda");
+                System.out.println("Vous avez choisi comme boisson : soda");
                 break;
             default:
-                System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+                System.out.println("Vous n'avez pas choisi de boisson parmi les choix proposés");
                 break;
         }
     }
